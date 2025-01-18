a8533f8df9e24e2b2efc13e1b596863246b6f64d
Shreyan <shreyanbhandari36@gmail.com>
2025-01-18 22:44:38 +0545

    Enhance banking app home page with new components and update dependencies
    - Added new components: HeaderBox, TotalBalanceBox, AnimatedCounter, DoughnutChart for improved UI.
    - Updated the home page to include user greeting and total balance display.
    - Introduced new dependencies: chart.js, react-chartjs-2, query-string, and react-countup for enhanced functionality.
    - Fixed typo in TotalBalanceBoxProps interface declaration.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
+const loggedInUser = {
+  firstName: "Yan",
+  lastName: "S",
+  email: "yan@voyage.com",
+  avatar: "/icons/logo.svg",
+}
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedInUser.firstName || 'Guest'}
+            subtext = "Access and Manage your account and transactions"
+          />
+        </header>
+
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={10000.35}
+          
+        />
+      </div>
+    </div>
   )
 }
 
